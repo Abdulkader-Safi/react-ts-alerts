d5830c2ae8a2b70ff68997acb07f83bad1ad3257
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-11-05 09:09:57 +0200

    build daynamic alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
-import "./App.css";
+import { AlertTriangle, Ban, Bell, CheckCheck, Info } from "lucide-react";
+import Alert from "./ui/Alert";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>YOU DON'T KNOW REACTJS</h1>
-      </div>
-    </>
+    <div style={{ margin: "2rem" }}>
+      <Alert type={"alert-default"} icon={<Bell size={20} />} title={"Upgrade your plan"}>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur temporibus doloremque
+          <a href="/">laboriosam</a> iste totam officiis beatae quas.
+        </p>
+      </Alert>
+
+      <Alert
+        type={"alert-info"}
+        icon={<Info size={20} />}
+        title={"Note"}
+        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur temporibus doloremque praesentium
+        laboriosam iste totam officiis beatae sint non quas."
+      />
+      <Alert
+        type={"alert-success"}
+        icon={<CheckCheck size={20} />}
+        title={"Your order has been processed"}
+        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur temporibus doloremque praesentium
+        laboriosam iste totam officiis beatae sint non quas."
+      />
+      <Alert
+        type={"alert-error"}
+        icon={<Ban size={20} />}
+        title={"Something went wrong"}
+        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur temporibus doloremque praesentium
+        laboriosam iste totam officiis beatae sint non quas."
+      />
+      <Alert
+        type={"alert-warning"}
+        icon={<AlertTriangle size={20} />}
+        title={"Tips & Tricks"}
+        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur temporibus doloremque praesentium
+        laboriosam iste totam officiis beatae sint non quas."
+      />
+    </div>
   );
 }
 
